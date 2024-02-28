2143b1eb78c1d59cfc13bafd7568aca596726a72
tomdv18 <tdellavecchia@fi.uba.ar>
2024-02-28 01:00:45 -0300

    Snake Terminada

--- snake.cpp
+++ snake.cpp
@@ -2,28 +2,48 @@
 #include <unistd.h>
 #include <ncurses.h>
 
+
 using namespace std;
 
-
+#define LENGTHTAIL 50
 
 const int width = 20;
-const int height = 20;
-
+const int height = 15;
 
 enum snakeDir {STOP = 0, LEFT,RIGHT, UP, DOWN};
 
-void setUp(bool *gameOver, int* x, int* y, int *fruitX,int*fruitY, int*score, snakeDir *dir){
-    *gameOver = false;
-    *dir = STOP;
-    *x = width/2;
-    *y = height/2;
-    *fruitX = rand()% width;
-    *fruitY = rand()% height;
-    *score =0;
+struct SnakeGame{
+    bool gameOver;
+    int x,y,fruitX,fruitY,score;
+    int tailX[LENGTHTAIL];
+    int tailY[LENGTHTAIL];
+    int nTail;
+    snakeDir dir;
 
-    
+};
+
+
+
+
+void pantallaFinal(int score){
+    cout << endl << "+-+-+-+-+ +-+-+-+-+" << endl;
+    cout << "|G|a|m|e| |o|v|e|r|" << endl;
+    cout << "+-+-+-+-+ +-+-+-+-+" << endl;
+
 }
-void draw(int x, int y, int fruitX, int fruitY, int score){
+
+void setUp(SnakeGame * snake) {
+    snake->gameOver = false;
+    snake->dir = STOP;
+    snake->x = width / 2;
+    snake->y = height / 2;
+    snake->fruitX = rand() % width - 1;
+    snake->fruitY = rand() % height- 1;
+    snake->score = 0;
+    snake->nTail = 0;
+
+}
+void draw(int x, int y, int fruitX, int fruitY, int score, int nTail, int tailX[LENGTHTAIL], int tailY[LENGTHTAIL]){
     system("clear");
     for (size_t i = 0; i < width; i++){
         cout << "#";
@@ -36,13 +56,21 @@ void draw(int x, int y, int fruitX, int fruitY, int score){
                 cout << "0";
             }
             else if (j==fruitY && k == fruitX){
-                cout << "o";
+                cout << "■";
             }
             else if (k== 0  || k == width -1){
                 cout << "#";
-            }         
-            else
-            cout << " ";
+            }          
+            else{
+                bool print = false;
+                for (size_t t = 0; t < nTail; t++){
+                    if ((tailX)[t] == k && (tailY)[t] == j){
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print) cout << " ";
+            }
         }
         cout << endl;
     }
@@ -56,7 +84,7 @@ void draw(int x, int y, int fruitX, int fruitY, int score){
     
 
 }
-#include <ncurses.h>
+
 
 void getInput(snakeDir * dir, bool *gameOver){
     initscr(); // Inicializa ncurses
@@ -85,49 +113,72 @@ void getInput(snakeDir * dir, bool *gameOver){
     endwin(); // Finaliza ncurses
 }
 
-void controller(snakeDir dir, int *x, int *y, bool *gameOver, int *fruitX, int* fruitY, int*score){
-    if (*x == *fruitX && *y == *fruitY){
-        cout << "Point Scored!" << endl;
-        *score = *score +1;
-        *fruitX = rand()% width;
-        *fruitY = rand()% height;
+void controller(SnakeGame * snake){
+    
+    int prevX = snake->x;
+    int prevY = snake->y;
+    int prev2X, prev2Y;
+    int temp = snake->tailX[0];
+    int temp2 = snake->tailY[0];
+    snake->tailX[0] = prevX;
+    snake->tailY[0] = prevY;
+
+    for(int i = 1; i < snake->nTail ; i++)
+    {
+        prev2X = snake->tailX[i];
+        prev2Y = snake->tailY[i];
+        snake->tailX[i] = temp;
+        snake->tailY[i] = temp2;
+        temp = prev2X;
+        temp2 = prev2Y;
     }
-    switch (dir)
+    switch (snake->dir)
     {
     case LEFT:
-        *x = *x - 1;
+        snake->x = snake->x - 1;
         break;
     case RIGHT:
-        *x= *x + 1;
+        snake->x= snake->x + 1;
         break;
     case UP:
-        *y = *y - 1;
+        snake->y = snake->y - 1;
         break;
     case DOWN:
-        *y= *y + 1;
+        snake->y= snake->y + 1;
         break;
     default:
         break;
     }
-    if (*x< 0 || *x > width ||  *y < 0 || *y > height){
-        *gameOver = true;
+    for (size_t k = 0; k < snake->nTail; k++){
+        if (snake->x == snake->tailX[k] && snake->tailY[k] == snake->y){
+            snake->gameOver = true;
+        }
+        
+    }
+    if (snake->x<= 0 || snake->x >= width ||  snake->y <=0 || snake->y >= height){
+        snake->gameOver = true;
     }
 
-    
+    if (snake->x == snake->fruitX && snake->y == snake->fruitY){
+        snake->score = snake->score +5;
+        snake->nTail = snake->nTail +1;
+        snake->fruitX = rand()% width  -1;
+        snake->fruitY = rand()% height -1;
+        cout << "Point Scored!" << endl;
+    }
 }
 int main(){
 
+    SnakeGame snake;
+    
+    setUp(&snake);
 
-    bool gameOver;
-    int x,y,fruitX,fruitY,score;
-    snakeDir dir;
-    setUp(&gameOver, &x,&y,&fruitX,&fruitY,&score, &dir);
-
-    while (!gameOver){
-        draw(x,y,fruitX,fruitY, score);   
-        getInput(&dir, &gameOver);
-        controller(dir, &x, &y, &gameOver, &fruitX, &fruitY, &score);
+    while (!(snake.gameOver)){
+        draw(snake.x,snake.y,snake.fruitX, snake.fruitY, snake.score, snake.nTail, snake.tailX, snake.tailY);   
+        getInput(&(snake.dir), &(snake.gameOver));
+        controller(&snake);
         sleep(1);
     }
+    pantallaFinal(snake.score);
     return 0;
 }
